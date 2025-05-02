d054a80f3f66e26448c85454d1e2161867be8c94
thefeky <162041853+thefeky@users.noreply.github.com>
2025-05-02 19:16:10 +0300

    fix: quick fix on fetching

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function Homepage() {
   return (
-    <main className="w-screen h-screen bg-[url(/src/assets/home/background-home-mobile.jpg)] md:bg-[url(/src/assets/home/background-home-tablet.jpg)] xl:bg-[url(/src/assets/home/background-home-desktop.jpg)] bg-cover bg-center overflow-y-auto relative pt-[88px] md:pt-[96px] xl:pt-[136px]">
+    <main className="w-screen h-screen bg-[url(/src/assets/home/background-home-mobile.jpg)] md:bg-[url(/src/assets/home/background-home-tablet.jpg)] xl:bg-[url(/src/assets/home/background-home-desktop.jpg)] bg-cover bg-center overflow-y-auto md:overflow-y-hidden relative pt-[88px] md:pt-[96px] xl:pt-[136px]">
       <div className="h-auto flex flex-col gap-6 items-center justify-center md:pt-16 xl:flex-row xl:pt-32">
         <div className="w-[90%] flex flex-col gap-4 text-center xl:text-start xl:gap-8 xl:w-[50%]">
           <p className="font-barlow-condensed text-[#D0D6F9] uppercase leading-none md:text-[28px] md:tracking-[4px]">
